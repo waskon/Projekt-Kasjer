21c5ed05e8742eef39cc77d243e61c641b264f86
Konrad Was <22konradwas22@gmail.com>
2021-06-13 16:15:00 +0200

    Dodanie obsługi przycisków numerycznych z wyświetleniem ich na ekran oraz przycisków czyszczacych i backspace

--- Interface.py
+++ Interface.py
@@ -29,59 +29,59 @@ class UserInterface:
                 numerical_btn_frame.grid(row=i, column=j, padx=5, pady=5)
                 if i == 3 and j == 0:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="Wyczyść", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.clear_entry())
                     numerical_btn.pack()
                 elif i == 3 and j == 2:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="Backspace", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.backspace_entry())
                     numerical_btn.pack()
                 elif i == 3 and j == 1:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="0", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("0"))
                     numerical_btn.pack()
                 elif i == 2 and j == 0:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="7", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("7"))
                     numerical_btn.pack()
 
                 elif i == 2 and j == 1:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="8", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("8"))
                     numerical_btn.pack()
 
                 elif i == 2 and j == 2:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="9", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("9"))
                     numerical_btn.pack()
 
                 elif i == 1 and j == 0:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="4", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("4"))
                     numerical_btn.pack()
 
                 elif i == 1 and j == 1:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="5", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("5"))
                     numerical_btn.pack()
 
                 elif i == 1 and j == 2:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="6", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("6"))
                     numerical_btn.pack()
 
                 elif i == 0 and j == 0:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="1", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("1"))
                     numerical_btn.pack()
 
                 elif i == 0 and j == 1:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="2", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("2"))
                     numerical_btn.pack()
 
                 elif i == 0 and j == 2:
                     numerical_btn = tk.Button(master=numerical_btn_frame, text="3", padx=15, pady=10,
-                                              font=fnt.Font(size=10))
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("3"))
                     numerical_btn.pack()
 
 
@@ -96,13 +96,13 @@ class UserInterface:
                               font=fnt.Font(size=10))
         weigh_btn.pack()
 
-# pole wyśw wpisane wyniki
+        # pole wyśw wpisane wyniki
         self.ent_weigh = tk.Entry(master=self.frame_right, font=fnt.Font(size=20), text="1")
         self.ent_weigh.place(width=300, height=50, relx=0.5, rely=0.15, anchor=tk.CENTER)
         self.ent_weigh.insert(0, 1)
         self.ent_weigh.configure(state=tk.DISABLED)
 
-# następny klient
+        # następny klient
         self.next_client_btn = tk.Button(master=self.frame_left, text="Następny klient", padx=15, pady=10, width=13,
                                          background="gray83",
                                          font=fnt.Font(size=10),)
@@ -111,3 +111,36 @@ class UserInterface:
         self.item_btn = tk.Button(master=self.frame_left, padx=15, pady=10, width=13, background="gray83",
                                   font=fnt.Font(size=10))
 
+    def add_number_to_entry(self, number):
+
+        self.ent_weigh.configure(state=tk.NORMAL)
+        if self.cleared_entry:
+            self.ent_weigh.delete(0, tk.END)
+            self.ent_weigh.insert(0, number)
+            self.cleared_entry = False
+        else:
+            current_text = self.ent_weigh.get()
+            self.ent_weigh.delete(0, tk.END)
+            self.ent_weigh.insert(0, current_text + number)
+
+        self.ent_weigh.configure(state=tk.DISABLED)
+
+    def clear_entry(self):
+
+        self.ent_weigh.configure(state=tk.NORMAL)
+        self.ent_weigh.delete(0, tk.END)
+        self.ent_weigh.insert(0, 1)
+        self.ent_weigh.configure(state=tk.DISABLED)
+        self.cleared_entry = True
+
+    def backspace_entry(self):
+
+        self.ent_weigh.configure(state=tk.NORMAL)
+        if len(self.ent_weigh.get()) == 1:
+            self.ent_weigh.delete(0, tk.END)
+            self.ent_weigh.insert(0, 1)
+        else:
+            self.ent_weigh.delete(len(self.ent_weigh.get()) - 1, tk.END)
+            self.cleared_entry = True
+
+        self.ent_weigh.configure(state=tk.DISABLED)
