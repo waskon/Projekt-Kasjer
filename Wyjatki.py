2d99d5192619c472297cc0b993894b6d9986ffb4
Konrad Was <22konradwas22@gmail.com>
2021-06-16 23:49:13 +0200

    Dodanie uzytych wyjatkow

---
+++ Wyjatki.py
@@ -0,0 +1,21 @@
+class Error(Exception):
+    pass
+
+
+class ItemUnweightedError(Error):
+    pass
+
+
+class ItemTooMuchError(Error):
+    pass
+
+
+class ItemTooMuchError(Error):
+    pass
+
+
+class TowarNaSztukiWeightedError(Error):
+    pass
+
+
+
